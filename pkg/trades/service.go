f793e59910b721bc4d80763ad7dddecfca5d3f35
Diego Oliveira <diego.oliveira@ficatranquilo.com.br>
2021-08-03 06:02:35 -0300

    adding log lines

--- pkg/trades/service.go
+++ pkg/trades/service.go
@@ -6,6 +6,7 @@ import (
 	"github.com/d-leme/tradew-trades/pkg/core"
 	"github.com/d-leme/tradew-trades/pkg/trades/external/inventory"
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 type service struct {
@@ -27,24 +28,38 @@ func (s *service) Create(
 	req *CreateTradeOfferRequest,
 ) (*CreateTradeOfferResponse, error) {
 
+	fields := logrus.Fields{
+		"user_id":               userID,
+		"wanted_items_owner_id": req.WantedItemsOwnerID,
+		"correlation_id":        correlationID,
+	}
+
 	offeredItems, err := ToDomain(req.OfferedItems)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error parsing offered items")
 		return nil, err
 	}
 
 	wantedItems, err := ToDomain(req.WantedItems)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error parsing wanted items")
 		return nil, err
 	}
 
 	trade, err := NewTradeOffer(uuid.NewString(), userID, req.WantedItemsOwnerID, offeredItems, wantedItems)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error creating new offer")
+
 		return nil, err
 	}
 
 	if err := s.repository.Insert(ctx, trade); err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error inserting offer")
 		return nil, err
 	}
+
+	fields["trade_id"] = trade.ID
+	logrus.WithFields(fields).Info("new trade created")
 
 	lockItemsReq := &inventory.LockItemsRequest{
 		LockedBy:           trade.ID,
@@ -69,11 +84,17 @@ func (s *service) Create(
 	}
 
 	if err := s.inventoryService.LockItems(ctx, lockItemsReq); err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error locking items")
+
 		trade.UpdateStatus(TradeError)
 
 		if err := s.repository.Update(ctx, trade); err != nil {
+			logrus.WithError(err).WithFields(fields).Error("error updating trade")
+
 			return nil, err
 		}
+
+		logrus.WithFields(fields).Info("trade status set to error")
 
 		return nil, core.ErrLockFailed
 	}
@@ -81,27 +102,46 @@ func (s *service) Create(
 	trade.UpdateStatus(TradePending)
 
 	if err := s.repository.Update(ctx, trade); err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error updating trade")
 		return nil, err
 	}
+
+	logrus.WithFields(fields).Info("trade status set to pending")
 
 	return &CreateTradeOfferResponse{ID: trade.ID}, nil
 }
 
 func (s *service) Accept(ctx context.Context, userID, correlationID, id string) error {
+
+	fields := logrus.Fields{
+		"trade_id":       id,
+		"user_id":        userID,
+		"correlation_id": correlationID,
+	}
+
 	trade, err := s.repository.GetByID(ctx, userID, id)
 	if err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error getting trade")
 		return err
 	}
 
 	if trade.Status != TradePending {
+		logrus.
+			WithError(core.ErrTradeInvalidStatus).
+			WithFields(fields).
+			Error("tried to accept trade that was in an invalid state")
+
 		return core.ErrTradeInvalidStatus
 	}
 
 	trade.UpdateStatus(TradeAccepted)
 
 	if err := s.repository.Update(ctx, trade); err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error updating trade")
 		return err
 	}
+
+	logrus.WithFields(fields).Info("trade status set to accepted")
 
 	tradeItemsReq := &inventory.TradeItemsRequest{
 		TradeID:            trade.ID,
@@ -130,8 +170,11 @@ func (s *service) Accept(ctx context.Context, userID, correlationID, id string) 
 		trade.UpdateStatus(TradeError)
 
 		if err := s.repository.Update(ctx, trade); err != nil {
+			logrus.WithError(err).WithFields(fields).Error("error updating trade")
 			return err
 		}
+
+		logrus.WithFields(fields).Info("trade status set to error")
 
 		return core.ErrItemsTradeFailed
 	}
@@ -139,8 +182,11 @@ func (s *service) Accept(ctx context.Context, userID, correlationID, id string) 
 	trade.UpdateStatus(TradeCompleted)
 
 	if err := s.repository.Update(ctx, trade); err != nil {
+		logrus.WithError(err).WithFields(fields).Error("error updating trade")
 		return err
 	}
+
+	logrus.WithFields(fields).Info("trade status set to completed")
 
 	return nil
 }
@@ -153,6 +199,13 @@ func (s *service) Get(ctx context.Context, userID string, req *GetTradeOffersReq
 	})
 
 	if err != nil {
+		logrus.
+			WithError(err).
+			WithFields(logrus.Fields{
+				"user_id": userID,
+				"token":   req.Token,
+			}).
+			Error("error getting trades")
 		return nil, err
 	}
 
@@ -163,6 +216,13 @@ func (s *service) GetByID(ctx context.Context, userID, id string) (*GetTradeOffe
 
 	trade, err := s.repository.GetByID(ctx, userID, id)
 	if err != nil {
+		logrus.
+			WithError(err).
+			WithFields(logrus.Fields{
+				"trade_id": id,
+				"user_id":  userID,
+			}).
+			Error("error getting trades")
 		return nil, err
 	}
 
